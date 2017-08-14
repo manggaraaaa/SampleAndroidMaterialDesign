1ad7e67ce828a256d50156647b007040bd2f1414
muhammadaa <muh.ari.anggara@gmail.com>
2017-08-14 14:04:59 +0700

    SampleMaterialDesign

---
+++ app/src/main/java/com/muhammadaa/samplematerialdesign/model/NavDrawerItem.java
@@ -0,0 +1,38 @@
+package com.muhammadaa.samplematerialdesign.model;
+
+/**
+ * Created by muhammadaa on 14/08/17.
+ */
+
+public class NavDrawerItem {
+
+    private boolean showNotify;
+    private String title;
+
+
+    public NavDrawerItem() {
+
+    }
+
+    public NavDrawerItem(boolean showNotify, String title) {
+        this.showNotify = showNotify;
+        this.title = title;
+    }
+
+    public boolean isShowNotify() {
+        return showNotify;
+    }
+
+    public void setShowNotify(boolean showNotify) {
+        this.showNotify = showNotify;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
